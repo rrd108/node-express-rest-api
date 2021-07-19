d69627fcfe5e6985a38734fb8cd76c30005b62f8
rrd <rrd@1108.cc>
2021-07-19 13:58:04 +0200

    index route added

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = 5000
 
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+  res.send('Gauranga')
+})
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
 })
