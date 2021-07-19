02c7d4e39cc763c90e7c00ca893bbf7a69cb3518
rrd <rrd@1108.cc>
2021-07-19 16:03:43 +0200

    cors added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import productsRoutes from './routes/products.js'
 import usersRoutes from './routes/users.js'
+import cors from 'cors'
 
 const app = express()
 const PORT = 5000
@@ -14,6 +15,14 @@ mongoose.connect('mongodb://localhost:27017/node-express-rest-api', {
 mongoose.connection
 
 app.use(bodyParser.json())
+app.use(
+  cors({
+    origin: true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Token'],
+  })
+)
+
 app.use('/products', productsRoutes)
 app.use('/users', usersRoutes)
 
